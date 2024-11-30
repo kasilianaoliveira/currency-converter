6c785c492677e6f8603c81c454771862388e2fea
kasilianaoliveira <kasilianaoliveira@gmail.com>
2024-11-30 17:23:20 -0300

    fix: adjust port configuration

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import uvicorn
+from decouple import config
+
 
 if __name__ == "__main__":
+    port = config("PORT", default=8000, cast=int)
     config = uvicorn.Config("src.app:app", port=5000, log_level="info", reload=True)
     server = uvicorn.Server(config)
     server.run()
