547a610fd686b124cd52da78f1bfeb7a343ecaa7
kasilianaoliveira <kasilianaoliveira@gmail.com>
2024-11-30 17:28:17 -0300

    feat: add host

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from decouple import config
 
 if __name__ == "__main__":
     port = config("PORT", default=8000, cast=int)
-    config = uvicorn.Config("src.app:app", port=5000, log_level="info", reload=True)
+    config = uvicorn.Config(
+        "src.app:app",
+        host="0.0.0.0",
+        port=5000,
+        log_level="info",
+        reload=True,
+    )
     server = uvicorn.Server(config)
     server.run()
